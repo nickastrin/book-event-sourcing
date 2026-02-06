4f998695dbc9ceb8d9797921151b056c2f5d05d9
Nikos Astrinakis <nikos.astrin@gmail.com>
2026-02-06 21:57:22 +0200

    Create a response model

---
+++ BookTrackerBackend/Features/Books/Models/BookResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace BookTracker.Api.Features.Books.Models;
+
+public class BookResponse
+{
+    public required string Title { get; set; }
+    public string Description { get; set; } = string.Empty; 
+    
+    public List<string> Authors { get; set; } = [];
+    public DateTime PublishDate { get; set; }
+}
