b8a7ddc76ba10db24234e374649651b6889f69d5
Nikos Astrinakis <nikos.astrin@gmail.com>
2026-02-06 21:15:00 +0200

    Add a base book controller

---
+++ BookTrackerBackend/Features/Books/Controllers/BookController.cs
@@ -0,0 +1,36 @@
+﻿
+using BookTracker.Api.Features.Books.Models;
+using BookTracker.Api.Features.Books.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookTracker.Api.Features.Books.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BookController(IBookService service) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<Book>> GetAllBooks(
+        [FromQuery] string? search, 
+        [FromQuery] int page = 1, 
+        [FromQuery] int pageSize = 10)
+    {
+        var result = await service.HandleGetAll(
+            search, page, pageSize);
+        
+        return Ok(result);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Book>> GetBookById(string id)
+    {
+        var result = await service.HandleGetById(id);
+
+        if (result == null)
+        {
+            return NotFound();
+        }
+        
+        return Ok(result);
+    }
+}
