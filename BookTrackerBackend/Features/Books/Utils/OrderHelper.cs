f933f8d0bb886fdc5c2cc3daad567a68dc858bf3
Nikos Astrinakis <nikos.astrin@gmail.com>
2026-02-07 15:22:30 +0200

    Add helpers for search and order

---
+++ BookTrackerBackend/Features/Books/Utils/OrderHelper.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq.Expressions;
+using BookTracker.Api.Features.Books.Models;
+
+namespace BookTracker.Api.Features.Books.Utils;
+
+public static class OrderHelper
+{
+    private static IQueryable<Book> ApplySort(
+        this IQueryable<Book> query,
+        Expression<Func<Book, object>> expression,
+        bool isDescending)
+    {
+        return isDescending ? 
+            query.OrderByDescending(expression) : 
+            query.OrderBy(expression);
+    }
+
+    public static IQueryable<Book> OrderQuery(this IQueryable<Book> query, string orderBy)
+    {
+        var values = orderBy.Split(" ");
+        var field = values[0].ToLower();
+        var isDescending = values.Length > 1 && 
+                           values[1].Equals("desc", StringComparison.CurrentCultureIgnoreCase);
+
+        return field switch
+        {
+            "title" => query.ApplySort(x => x.Title.ToLower(), isDescending),
+            "description" => query.ApplySort(x => x.Description.ToLower(), isDescending),
+            "publishdate" => query.ApplySort(x => x.PublishDate, isDescending),
+            _ => query.ApplySort(x => x.Title.ToLower(), isDescending)
+        };
+    }
+}
---
+++ BookTrackerBackend/Features/Books/Utils/SearchHelper.cs
@@ -0,0 +1,18 @@
+﻿using BookTracker.Api.Features.Books.Models;
+
+namespace BookTracker.Api.Features.Books.Utils;
+
+public static class SearchHelper
+{
+    public static IQueryable<Book> ApplySearch(this IQueryable<Book> query, string search)
+    {
+        if (string.IsNullOrEmpty(search))
+        {
+            return query;
+        }
+        
+        return query.Where(b => 
+            b.Title.Contains(search, StringComparison.CurrentCultureIgnoreCase) ||
+            b.Description.Contains(search, StringComparison.CurrentCultureIgnoreCase));
+    }
+}
