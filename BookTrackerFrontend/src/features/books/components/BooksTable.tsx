404c66190483905d34b8b881ad4066c2ab99de1a
Nikos Astrinakis <nikos.astrin@gmail.com>
2026-02-07 17:35:55 +0200

    Use new helper function

--- BookTrackerFrontend/src/features/books/components/BooksTable.tsx
+++ BookTrackerFrontend/src/features/books/components/BooksTable.tsx
@@ -2,8 +2,8 @@ import type { ColumnDef } from "@tanstack/react-table";
 import { useGetAll } from "../queries";
 import type { BookService } from "../services";
 import type { Book } from "../types";
-import { Grid } from "../../../views";
-import { useFilterContext } from "../../../core";
+import { Grid } from "@src/views";
+import { createSearchParams, useFilterContext } from "@src/core";
 import { useMemo } from "react";
 
 interface BooksTableProps {
@@ -12,21 +12,10 @@ interface BooksTableProps {
 
 export const BooksTable = ({ service }: BooksTableProps) => {
   const { queryFilters } = useFilterContext();
-  const queryParams = useMemo(() => {
-    const params = new URLSearchParams();
-    if (queryFilters.search) {
-      params.append("search", queryFilters.search);
-    }
-    if (queryFilters.pagination) {
-      params.append("page", queryFilters.pagination.page.toString());
-      params.append("pageSize", queryFilters.pagination.pageSize.toString());
-    }
-    if (queryFilters.orderBy) {
-      params.append("orderBy", queryFilters.orderBy);
-    }
-
-    return params.toString();
-  }, [queryFilters]);
+  const queryParams = useMemo(
+    () => createSearchParams(queryFilters),
+    [queryFilters],
+  );
 
   const { data, totalCount } = useGetAll({
     params: queryParams,
