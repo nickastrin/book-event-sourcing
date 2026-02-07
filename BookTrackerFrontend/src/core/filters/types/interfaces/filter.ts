67852b8c1e1747c9a84a6add7ed4bb1dd5a5e38f
Nikos Astrinakis <nikos.astrin@gmail.com>
2026-02-07 17:27:57 +0200

    Remove operators

--- BookTrackerFrontend/src/core/filters/types/interfaces/filter.ts
+++ BookTrackerFrontend/src/core/filters/types/interfaces/filter.ts
@@ -1,7 +1,4 @@
-import type { ComparisonOperator } from "../enums/operators";
-
 export interface Filter {
   key: string;
-  operator: ComparisonOperator;
-  value: unknown;
+  value: string | string[];
 }
