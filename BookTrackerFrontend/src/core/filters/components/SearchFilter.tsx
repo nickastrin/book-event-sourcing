9a66160c642ce3b11cc10afd62b224db0a2a1724
Nikos Astrinakis <nikos.astrin@gmail.com>
2026-02-07 17:30:21 +0200

    Create search filter component

---
+++ BookTrackerFrontend/src/core/filters/components/SearchFilter.tsx
@@ -0,0 +1,39 @@
+import { useMemo, useState } from "react";
+import { useFilterContext } from "../contexts";
+import { DEFAULT_PAGINATION } from "../types";
+import { debounce } from "lodash";
+
+export const SearchFilter = () => {
+  const { queryFilters, onUpdateQueryFilters } = useFilterContext();
+
+  const [search, setSearch] = useState(queryFilters.search || "");
+
+  const debouncedUpdate = useMemo(
+    () =>
+      debounce((value: string) => {
+        onUpdateQueryFilters({
+          search: value,
+          pagination: {
+            ...(queryFilters.pagination || DEFAULT_PAGINATION),
+            page: 1,
+          },
+        });
+      }, 500),
+    [onUpdateQueryFilters, queryFilters.pagination],
+  );
+
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setSearch(value);
+    debouncedUpdate(value);
+  };
+
+  return (
+    <input
+      type="text"
+      value={search}
+      onChange={onInputChange}
+      placeholder="Search..."
+    />
+  );
+};
