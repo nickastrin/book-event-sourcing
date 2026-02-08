516c9b344cb746c129bd4f4bf0ea2bea1e03861b
Nikos Astrinakis <nikos.astrin@gmail.com>
2026-02-08 13:57:55 +0200

    Use the new modal in the list page

--- BookTrackerFrontend/src/pages/BooksListPage.tsx
+++ BookTrackerFrontend/src/pages/BooksListPage.tsx
@@ -1,13 +1,41 @@
+import { useState } from "react";
 import { FilterProvider } from "../core";
-import { BookService, BooksFilterbar, BooksTable } from "../features/books";
+import {
+  BookService,
+  BooksFilterbar,
+  BooksTable,
+  BooksUpsertModal,
+} from "../features/books";
+import { useCreate } from "@src/features/books/queries";
 
 export const BooksListPage = () => {
   const service = new BookService();
+  const { createEntry } = useCreate({ service });
+
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <FilterProvider>
       <BooksFilterbar />
       <BooksTable service={service} />
+
+      <button
+        className="fixed bottom-0 right-0 m-4 flex justify-center icon"
+        onClick={() => setShowModal(true)}
+      >
+        <span className="material-symbols-outlined block">add</span>
+      </button>
+
+      <BooksUpsertModal
+        show={showModal}
+        onClose={() => setShowModal(false)}
+        onSubmit={async (data) => {
+          const id = await createEntry({ data });
+          setShowModal(false);
+
+          return id;
+        }}
+      />
     </FilterProvider>
   );
 };
